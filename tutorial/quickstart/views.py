52f0fb6100bb139214d27f3648ebaf61366f987b
Igor Eduardo Mascarenhas <iem@cin.ufpe.br>
2021-10-14 22:43:54 -0300

    Feito Tutorial do link que está no README

---
+++ tutorial/quickstart/views.py
@@ -0,0 +1,23 @@
+from django.contrib.auth.models import User, Group
+from rest_framework import viewsets
+from rest_framework import permissions
+from tutorial.quickstart.serializers import UserSerializer, GroupSerializer
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows users to be viewed or edited.
+    """
+    #se não quiser fazer nada de especial com a query set pode deixar a implementaçao do proprio restframework
+    queryset = User.objects.all().order_by('-date_joined')
+    serializer_class = UserSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+
+class GroupViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows groups to be viewed or edited.
+    """
+    queryset = Group.objects.all()
+    serializer_class = GroupSerializer
+    permission_classes = [permissions.IsAuthenticated]
